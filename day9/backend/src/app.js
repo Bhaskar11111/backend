70033dc50d22f301088d698ed602fdc1d6352d05
Bhaskar Mishra <bhaskarmishra911@gmail.com>
2026-03-03 02:35:22 +0530

    day9

---
+++ day9/backend/src/app.js
@@ -0,0 +1,63 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+const path=require('path')
+app.use(express.json())
+app.use(cors())
+app.use(express.static('./public'))
+const noteModel=require('./model/notes.model')
+
+
+
+app.post('/api/notes',async(req,res)=>
+{
+    const data=req.body;
+    const notes=await noteModel.create({
+        title:data.title,
+        status:data.status,
+        description:data.description,
+        age:data.age,
+    })
+    res.status(201).json({
+        message:'Note created successfully',
+        notes
+    })
+})
+
+app.get('/api/notes',async(req,res)=>
+{
+    const notes=await noteModel.find()
+    res.status(200).json({
+        message:'Notes fetched successfully',
+        notes
+    })
+})
+
+app.patch('/api/notes/:id',async(req,res)=>
+{
+    const id=req.params.id;
+    const data=req.body
+    await noteModel.findByIdAndUpdate(id,{
+        title:data.title,
+        description:data.description,
+    })
+    res.status(201).json({
+        message:'Note updated successfully'
+    })
+})
+
+app.delete('/api/notes/:id',async(req,res)=>
+{
+    const id=req.params.id;
+    await noteModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message:'Note deleted successfully'
+    })
+})
+
+app.use('*name',(req,res)=>
+{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
+
+module.exports=app;
