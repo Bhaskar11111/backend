0f2904f48119474bad4e056c469d7f37c09889d9
Bhaskar Mishra <bhaskarmishra911@gmail.com>
2026-03-10 10:29:39 +0530

    day13

---
+++ day13/src/app.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+const authRouter=require('./routes/auth.route')
+const cookieParser=require('cookie-parser')
+
+app.use(cookieParser())
+app.use('/authentication/api',authRouter)
+
+
+module.exports=app
