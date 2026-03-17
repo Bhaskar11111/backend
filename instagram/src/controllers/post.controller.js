a8998d177701208c0f7342611d39c99a0577c943
Bhaskar Mishra <bhaskarmishra911@gmail.com>
2026-03-18 01:34:40 +0530

    til post details

--- instagram/src/controllers/post.controller.js
+++ instagram/src/controllers/post.controller.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const postModel=require('../models/post.model')
+const jwt=require('jsonwebtoken')
 const ImageKit=require('@imagekit/nodejs')
 const {toFile}=require('@imagekit/nodejs')
-const jwt=require('jsonwebtoken')
+
 const client=new ImageKit({
     privateKey:process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 const createPostController=(async(req,res)=>
-{
+{ 
     const token=req.cookies.token
     if(!token){
         return res.status(401).json({
@@ -16,37 +17,112 @@ const createPostController=(async(req,res)=>
         })
     }
     
-    let decoded=null
+    let decoded
+   try{ 
+     decoded=jwt.verify(token,process.env.JWT_SECRET)}
+   catch(err){
 
-  try{
-      decoded=jwt.verify(token,process.env.JWT_SECRET)
-  }
-  catch(err){
-    return res.status(401).json({
-        message:'Unauthorized access',
-        err
-    })
-  }
-    console.log(decoded)
-    
+   }
+    const userId=decoded.id
+    console.log(userId)
+
     const response=await client.files.upload({
         file:await toFile(Buffer.from(req.file.buffer),'file'),
-        fileName:'fileName',
-        folder:'cohort-2-i-c'
+        fileName:req.file.originalname,
+        folder:'instagram2'
     })
 
     const post=await postModel.create({
         caption:req.body.caption,
         imageUrl:response.url,
-        user:decoded.id
+        user:userId
+
     })
 
     res.status(200).json({
         message:'Post created successfully',
-        post 
+        post
     })    
 })
 
+const getPostController=(async(req,res)=>
+{
+    const token=req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:'Unauthorized access'
+        })
+    }
+
+    let decoded
+    try{
+        decoded=jwt.verify(token,process.env.JWT_SECRET)
+    }
+    catch(err){
+        return res.status(404).json({
+            message:'Invalid user'
+        })
+    }
+
+    const userId=decoded.id
+
+    const post=await postModel.findOne({
+        user:userId
+    })
+
+    res.status(200).json({
+        message:'Post fetched successfully',
+        post
+    })
+})
+
+const getPostDetailsController=(async(req,res)=>
+{
+    const token=req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:'Unauthorized access'
+        })
+    }
+
+    let decoded
+    try{
+        decoded=jwt.verify(token,process.env.JWT_SECRET)
+    }
+    catch(err){
+        return res.status(401).json({
+            message:'Invalid token'
+        })
+    }
+    console.log(decoded)
+
+    const userId=decoded.id
+    const postId=req.params.postId
+
+    const post=await postModel.findById(postId)
+    if(!post){
+        return res.status(404).json({
+            message:'Post not found'
+        })
+    }
+
+    const isValidUser=post.user.toString()===userId.toString()
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:'Forbidden content'
+        })
+    }
+
+    res.status(200).json({
+        message:'Post fetched successfully',
+        post
+    })
+})
+
 module.exports={
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
